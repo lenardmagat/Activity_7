b2eb318bd2ac0d1e9d532e795cdcfe661358ff97
lenardmagat <lenardmagat76@gmail.com>
2026-06-03 11:50:38 +0800

    added getter and setter method each private data

--- The_fan_class/Fan_class.py
+++ The_fan_class/Fan_class.py
@@ -18,6 +18,41 @@ class Fan:
             return ReturnValue.failure("Speed must be Slow(1), Medium(2), Fast(3)")
         self.__speed = value
     
+    @property
+    def speed(self) -> ReturnValue:
+        ReturnValue.success(self.__speed)
+    
+    @on.setter
+    def on(self, value: bool):
+        if not isinstance(value, bool):
+            return ReturnValue.failure("On must be a bool")
+        self.__on = self.on
+
+    @property
+    def on(self) -> ReturnValue:
+        ReturnValue.success(self.__on)
+         
+    @radius.setter
+    def radius(self, value: float):
+        if not isinstance(value, float):
+            return ReturnValue.failure("Radius must be a float")
+        if value < 0:
+            return ReturnValue.failure("Radius must be greater than 0")
+        self.__radius = self.radius
+    
+    @property
+    def radius(self) -> ReturnValue:
+        return ReturnValue.success(self.__radius)
+    
+    @color.setter
+    def color(self, value):
+        if not isinstance(value, str):
+            return ReturnValue.failure("color must be string")
+        self.__color = self.color
+
+    @property
+    def color(self) -> ReturnValue:
+        return ReturnValue.success(self.__color)
     
 
 

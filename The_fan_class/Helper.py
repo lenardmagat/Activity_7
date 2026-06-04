b5869a9ef514c3689d5ed35b32180ca8fa947f37
lenardmagat <lenardmagat76@gmail.com>
2026-06-04 20:37:18 +0800

    create car class

--- The_fan_class/Helper.py
+++
@@ -1,6 +0,0 @@
-from Schema import ReturnValueSchema
-class ReturnValue:
-    def success(self, value) -> ReturnValueSchema:
-        return  ReturnValueSchema(is_success = False, value = value, message = None)
-    def failure(self, message) -> ReturnValueSchema:
-        return  ReturnValueSchema(is_success = False, value = None, message = message)    
--- The_fan_class/TestFan.py
+++
@@ -1,4 +0,0 @@
-from Fan_class import Fan, FanData
-fan1_data = FanData(speed = 3, radius = 10, color = "yellow", on = True) 
-fan1 = Fan(fan1_data)
-print(fan1.color, fan1.speed, fan1.radius, fan1.on)
